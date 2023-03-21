d84bf80a66dda3bcf4c92ecb8793f07d55e209fd
pohjie <0xArlet@gmail.com>
2023-03-21 13:21:16 +0800

    Allow user to sign up

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const session = require("express-session");
-const passport = require("./passport");
+const passport = require("passport");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const { body, validationResult } = require("express-validator");
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +8,48 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/signup', function(req, res, next) {
+  res.render('sign-up-form', { title: 'Sign up'});
+});
+
+router.post('/signup', [
+  body("email", "Email must not be empty.")
+    .trim()
+    .isEmail(),
+  body("password", "Password must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("passwordCfm")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .custom((val, { req }) => val === req.body.password)
+    .withMessage("The passwords do not match"),
+
+  async (req, res, next) => {
+    const user = { email: req.body.email, password: req.body.password };
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('sign-up-form', {
+        title: 'Sign up',
+        user,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Valid form fields, call API to save
+    axios.post('http://localhost:3000/users/signup', user)
+      .then(response => {
+        res.redirect("/");
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("/");
+      });
+  }
+]);
 
 module.exports = router;
